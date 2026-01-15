889e63610a93b61c30898024ee713691a14309f2
manueljose199-spec <manueljoseemma199@gmail.com>
2026-01-14 22:56:29 -0600

    feat: se integro función para agregar paradas al hacer clic en el mapa

--- javascript.js
+++ javascript.js
@@ -5,3 +5,38 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let points = [];
 let line;
+
+
+map.on('click', e => {
+    let marker = L.marker(e.latlng).addTo(map);
+    points.push(e.latlng);
+    document.getElementById("stops").textContent = points.length;
+    
+    if (points.length > 1) {
+        if (line) map.removeLayer(line);
+        line = L.polyline(points, {
+            color: '#2f3e9e',
+            weight: 3,
+            opacity: 0.7
+        }).addTo(map);
+        document.getElementById("distance").textContent = 
+            ((points.length - 1) * 5).toFixed(1) + " km";
+    }
+});
+
+map.on('click', e => {
+    let marker = L.marker(e.latlng).addTo(map);
+    points.push(e.latlng);
+    document.getElementById("stops").textContent = points.length;
+    
+    if (points.length > 1) {
+        if (line) map.removeLayer(line);
+        line = L.polyline(points, {
+            color: '#2f3e9e',
+            weight: 3,
+            opacity: 0.7
+        }).addTo(map);
+        document.getElementById("distance").textContent = 
+            ((points.length - 1) * 5).toFixed(1) + " km";
+    }
+});
