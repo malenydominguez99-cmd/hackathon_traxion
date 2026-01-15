b6676d0bdb1adc3b285b5bda2ced9d08aa268469
manueljose199-spec <manueljoseemma199@gmail.com>
2026-01-14 23:12:26 -0600

    feat: agregar handler principal del mensaje del chat

--- javascript.js
+++ javascript.js
@@ -24,23 +24,6 @@ map.on('click', e => {
     }
 });
 
-map.on('click', e => {
-    let marker = L.marker(e.latlng).addTo(map);
-    points.push(e.latlng);
-    document.getElementById("stops").textContent = points.length;
-    
-    if (points.length > 1) {
-        if (line) map.removeLayer(line);
-        line = L.polyline(points, {
-            color: '#2f3e9e',
-            weight: 3,
-            opacity: 0.7
-        }).addTo(map);
-        document.getElementById("distance").textContent = 
-            ((points.length - 1) * 5).toFixed(1) + " km";
-    }
-});
-
 let chatState = "INIT";
 let viaje = { origen: "", destino: "" };
 const chatContainer = document.getElementById("chat");
@@ -75,3 +58,6 @@ function send() {
 
 }
 
+function handleChat(t) {
+    
+}
